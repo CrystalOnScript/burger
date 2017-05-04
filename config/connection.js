7e507ac8190a2bf1c90be1f9bcd1171c2260fab0
CrystalFaith <crystallambert@Crystals-MacBook-Pro.local>
2017-05-04 08:25:57 -0700

    updated mysql deploy

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "",
   database: "eat_burger_db"
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
