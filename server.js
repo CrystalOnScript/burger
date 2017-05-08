3692c20b442d7e52ced87d9928c7f5466412793e
CrystalFaith <crystallambert@Crystals-MacBook-Pro.local>
2017-05-08 15:35:14 -0700

    update port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
